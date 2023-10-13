724651f2695cf790a02703176d2e35e6bf65ecc9
fergoh783 <fergoh783@example.com>
2023-10-13 21:11:37 +0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { Extension, type, api } = require('clipcc-extension');
 
 class MyExtension extends Extension {
-    whatPlanIsUsedWhenHitError = "b"
+    whatPlanIsUsedWhenHitError = "b";
+    capturedError = undefined;
     logError(e) {
         //错误处理相关实现借鉴自：github.com/bddjr/clipcc-extension-nhjrToolBox/blob/1fb1859cd3ac44cf9ceade46b40adde2a24153cb/index.js#L10
-        String()
+        console.error(e);
+        this.capturedError = String(e);
+        if (this.whatPlanIsUsedWhenHitError === 'a') {
+            return String(e);
+        }
         return 0;
     }
     //用于处理用户输入的图片 URL。
@@ -26,7 +31,13 @@ class MyExtension extends Extension {
             messageId: 'jexjws.mediasession.block.can_use_mediaSession',
             categoryId: 'jexjws.mediasession.category.welcome',
             type: type.BlockType.BOOLEAN,
-            function: () => ("mediaSession" in navigator)
+            function: () => {
+                try {
+                    return ("mediaSession" in navigator);
+                } catch (e) {
+                    return this.logError(e)
+                }
+            }
         })
         /*积木 "当执行出错时：" ：
         直接通过积木块返回报错信息（易于调试）+ 记录报错信息到 "mediaSession 报错信息" 积木中 + console报错
@@ -52,10 +63,17 @@ class MyExtension extends Extension {
                 }
             },
             function: () => {
-                try { whatPlanIsUsedWhenHitError = args.what_plan }
-                catch (e) { return logError(e) }
+                try {
+                    throw "test";
+                    whatPlanIsUsedWhenHitError = args.what_plan;
+                }
+                catch (e) { return this.logError(e); }
             }
         })
+        //积木 "mediaSession 是否可用" ：
+
+
+
         //分类 "媒体元数据" ：
         api.addCategory({
             categoryId: 'jexjws.mediasession.category.media_metadata',
@@ -64,11 +82,18 @@ class MyExtension extends Extension {
         });
         //积木 “初始化：媒体元数据”
         api.addBlock({
-            opcode: 'jexjws.mediasession.v2.set_media_metadata',
+            opcode: 'jexjws.mediasession.v1.set_media_metadata',
             type: type.BlockType.COMMAND,
             messageId: 'jexjws.mediasession.block.reset_media_metadata',
             categoryId: 'jexjws.mediasession.category.media_metadata',
-            function: () => 0
+            function: () => {
+                try {
+                    navigator.mediaSession.metadata = new MediaMetadata();
+                } catch (e) {
+                    return this.logError(e);
+                }
+
+            }
         })
         //积木 "设定媒体元数据中的 [key] 为 [value] " ：
         api.addBlock({
@@ -96,7 +121,7 @@ class MyExtension extends Extension {
                     default: '  '
                 }
             },
-            function: (args) => 'Hello, ClipCC!' + String(JSON.stringify(args)+this.whatPlanIsUsedWhenHitError)
+            function: (args) => 'Hello, ClipCC!' + String(JSON.stringify(args) + this.whatPlanIsUsedWhenHitError)
         });
     }
     onUninit() {
@@ -104,5 +129,6 @@ class MyExtension extends Extension {
         api.removeCategory('jexjws.mediasession.category.compatibility_test');
     }
 }
+
 
 module.exports = MyExtension;
